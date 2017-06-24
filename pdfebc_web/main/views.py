fa0d62dedb57065be9c5d7b36edf2eed32286a43
Simon Larsen <slarse@kth.se>
2017-06-24 15:49:36 +0200

    Add functionality for uploading multiple files

--- pdfebc_web/main/views.py
+++ pdfebc_web/main/views.py
@@ -11,8 +11,7 @@ import os
 import tempfile
 import uuid
 import tarfile
-from collections import defaultdict
-from flask import render_template, send_file, session
+from flask import render_template, send_file, session, flash
 from werkzeug import secure_filename
 from pdfebc_core import compress
 from . import main
@@ -20,14 +19,11 @@ from .forms import FileUploadForm, CompressFilesForm
 
 PDFEBC_CORE_GITHUB = 'https://github.com/slarse/pdfebc-core'
 PDFEBC_WEB_GITHUB = 'https://github.com/slarse/pdfebc-web'
-UPLOADED_FILES_KEY = 'uploaded_files'
-SESSION_UPLOAD_DIR_KEY = 'upload_dir'
-TEMPORARY_DIRECTORY = tempfile.gettempdir()
-UPLOAD_DIRECTORY = os.path.join(TEMPORARY_DIRECTORY, 'pdfebc-web')
-os.makedirs(UPLOAD_DIRECTORY, exist_ok=True)
+
+FILE_CACHE = os.path.join(tempfile.gettempdir(), 'pdfebc-web')
+os.makedirs(FILE_CACHE, exist_ok=True)
+
 SESSION_ID_KEY = 'session_id'
-UPLOAD_DIR_SUFFIX = 'pdfebc-web'
-UPLOADED_FILES = defaultdict(list)
 
 def make_tarfile(source_dir, out):
     if not out.endswith('.tgz'):
@@ -36,19 +32,19 @@ def make_tarfile(source_dir, out):
         tar.add(source_dir, arcname=os.path.basename(source_dir))
 
 
-def compress_and_serve_uploaded_files():
-    session_id = session[SESSION_ID_KEY]
+def compress_uploaded_files(src_dir):
+    """Compress the files in src_dir and place in an archive.
+
+    Args:
+        src_dir (str): Path to the source directory.
+    Returns:
+        str: Path to a tarball with the compressed files.
+    """
     with tempfile.TemporaryDirectory() as tmpdir:
-        for filename, file_contents in UPLOADED_FILES[session_id]:
-            src_file = open(os.path.join(tmpdir, filename), 'wb')
-            src_file.write(file_contents)
-            src_file.close()
-        with tempfile.TemporaryDirectory() as tmp_out_dir:
-            compress.compress_multiple_pdfs(tmpdir, tmp_out_dir, 'gs')
-            out = os.path.join(tmp_out_dir, 'compressed_files.tgz')
-            make_tarfile(tmp_out_dir, out)
-            UPLOADED_FILES[session_id] = []
-            return send_file(out)
+        compress.compress_multiple_pdfs(src_dir, tmpdir, 'gs')
+        out = os.path.join(src_dir, 'compressed_files.tgz')
+        make_tarfile(tmpdir, out)
+    return out
 
 
 def create_session_upload_dir(session_id):
@@ -57,7 +53,7 @@ def create_session_upload_dir(session_id):
     Args:
         session_id (str): The id for the session.
     """
-    directory = os.path.join(UPLOAD_DIRECTORY, session_id)
+    directory = get_session_upload_dir_path(session_id)
     os.mkdir(directory)
 
 
@@ -67,7 +63,7 @@ def get_session_upload_dir_path(session_id):
     Args:
         session_id (str): The id for the session.
     """
-    return os.path.join(UPLOAD_DIRECTORY, session_id)
+    return os.path.join(FILE_CACHE, session_id)
 
 
 def session_upload_dir_exists(session_id):
@@ -86,16 +82,22 @@ def index():
     test_form = CompressFilesForm()
     form = FileUploadForm()
     if SESSION_ID_KEY not in session:
-        session[SESSION_ID_KEY] = uuid.uuid4()
+        session[SESSION_ID_KEY] = str(uuid.uuid4())
     session_id = session[SESSION_ID_KEY]
+    session_upload_dir_path = get_session_upload_dir_path(session_id)
+    if not session_upload_dir_exists(session_id):
+        create_session_upload_dir(session_id)
     if form.validate_on_submit():
         file = form.upload.data
         filename = secure_filename(file.filename)
-        UPLOADED_FILES[session_id].append((secure_filename(file.filename), file.read()))
+        file.save(
+            os.path.join(session_upload_dir_path, filename))
+        flash("File {} was uploaded!".format(filename))
     if test_form.validate_on_submit():
-        return compress_and_serve_uploaded_files()
+        tar = compress_uploaded_files(session_upload_dir_path)
+        return send_file(tar, as_attachment=True)
     return render_template('index.html', form=form,
-                           uploaded_files=[file[0] for file in UPLOADED_FILES[session_id]],
+                           uploaded_files=[file for file in os.listdir(session_upload_dir_path)],
                            test_form=test_form)
 
 @main.route('/about')
