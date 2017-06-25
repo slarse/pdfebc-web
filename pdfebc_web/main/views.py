2ff9e6d388252dbb320f7a29cfb3d6b0e6621368
Simon Larsén <slarse@kth.se>
2017-06-25 17:53:26 +0200

    Send results by email, fix #7 (PR #8)
    * Remove Celery example function and endpoint
    
    * Add celery task for compressing and sending results by email
    
    * Fix bug in file sending and change cache location
    
    * Send files as separately attached pdfs instead of .tgz

--- pdfebc_web/main/views.py
+++ pdfebc_web/main/views.py
@@ -7,22 +7,20 @@
 
 .. moduleauthor:: Simon Larsén <slarse@kth.se>
 """
-import time
 import os
 import shutil
 import tempfile
 import uuid
 import tarfile
-from celery import Celery
-from flask import render_template, send_file, session, flash, Blueprint, redirect, url_for
+from pdfebc_core import email_utils, compress, config_utils
+from flask import render_template, session, flash, Blueprint, redirect, url_for
 from werkzeug import secure_filename
-from pdfebc_core import compress
 from .forms import FileUploadForm, CompressFilesForm
 
 PDFEBC_CORE_GITHUB = 'https://github.com/slarse/pdfebc-core'
 PDFEBC_WEB_GITHUB = 'https://github.com/slarse/pdfebc-web'
 
-FILE_CACHE = os.path.join(tempfile.gettempdir(), 'pdfebc-web')
+FILE_CACHE = os.path.join(os.path.dirname(config_utils.CONFIG_PATH), 'pdfebc-web')
 os.makedirs(FILE_CACHE, exist_ok=True)
 
 SESSION_ID_KEY = 'session_id'
@@ -43,8 +41,8 @@ def make_tarfile(src_dir, out):
         tar.add(src_dir, arcname=os.path.basename(src_dir))
 
 
-def compress_uploaded_files(src_dir):
-    """Compress the files in src_dir and place in an archive.
+def compress_uploaded_files_to_tgz(src_dir, status_callback=None):
+    """Compress the files in src_dir and place in a comrpessed tarball.
 
     Args:
         src_dir (str): Path to the source directory.
@@ -52,10 +50,24 @@ def compress_uploaded_files(src_dir):
         str: Path to a tarball with the compressed files.
     """
     with tempfile.TemporaryDirectory() as tmpdir:
-        compress.compress_multiple_pdfs(src_dir, tmpdir, 'gs')
+        compress.compress_multiple_pdfs(src_dir, tmpdir, 'gs', status_callback=status_callback)
         out = os.path.join(src_dir, 'compressed_files.tgz')
         make_tarfile(tmpdir, out)
     return out
+
+
+def compress_uploaded_files(src_dir, status_callback=None):
+    """Compress the pdf files in the given source directory and place them in a
+    subdirectory.
+
+    Args:
+        src_dir (str): Path to the source directory.
+    Returns:
+        List[str]: Paths to the compressed files.
+    """
+    out_dir = os.path.join(src_dir, 'compressed_files')
+    os.mkdir(out_dir)
+    return compress.compress_multiple_pdfs(src_dir, out_dir, 'gs', status_callback=status_callback)
 
 
 def create_session_upload_dir(session_id):
@@ -87,7 +99,7 @@ def session_upload_dir_exists(session_id):
     return os.path.isdir(directory)
 
 
-def clear_session_upload_dir(session_id):
+def delete_session_upload_dir(session_id):
     """Remove all files in the session upload directory.
 
     Args:
@@ -108,18 +120,34 @@ def tarball_in_session_upload_dir(session_id):
             any(map(lambda filename: filename.endswith('.tgz'), os.listdir(session_upload_dir)))
 
 def construct_blueprint(celery):
+    """Construct the main blueprint.
+
+    Args:
+        celery (Celery): A Celery instance.
+    Returns:
+        Blueprint: A Flask Blueprint.
+    """
     main = Blueprint('main', __name__)
 
     @celery.task
-    def stupid_background_task():
-        for _ in range(10):
-            print('BACKGROUND TASK')
-            time.sleep(2)
+    def process_uploaded_files(session_id):
+        """Compress the files uploaded to the session upload directory and send them
+        by email with the preconfigured values in the pdfebc-core config.
+
+        Also clears the session upload directory when done.
+
+        Args:
+            session_id (str): Id of the session.
+        """
+        session_upload_dir = get_session_upload_dir_path(session_id)
+        filepaths = compress_uploaded_files(session_upload_dir)
+        email_utils.send_files_preconf(filepaths)
+        delete_session_upload_dir(session_id)
 
     @main.route('/', methods=['GET', 'POST'])
     def index():
         """View for the index page."""
-        test_form = CompressFilesForm()
+        compress_form = CompressFilesForm()
         form = FileUploadForm()
         if SESSION_ID_KEY not in session:
             session[SESSION_ID_KEY] = str(uuid.uuid4())
@@ -127,29 +155,21 @@ def construct_blueprint(celery):
         session_upload_dir_path = get_session_upload_dir_path(session_id)
         if not session_upload_dir_exists(session_id):
             create_session_upload_dir(session_id)
-        if tarball_in_session_upload_dir(session_id):
-            clear_session_upload_dir(session_id)
         if form.validate_on_submit():
             file = form.upload.data
             filename = secure_filename(file.filename)
             file.save(
                 os.path.join(session_upload_dir_path, filename))
             flash("{} was successfully uploaded!".format(filename))
-        if test_form.validate_on_submit():
-            tar = compress_uploaded_files(session_upload_dir_path)
-            return send_file(tar, as_attachment=True)
+        if compress_form.validate_on_submit():
+            process_uploaded_files.delay(session_id)
+            flash("Your files are being compressed and will be sent by email upon completion.")
+            return redirect(url_for('main.index'))
+        uploaded_files = [] if not os.path.isdir(session_upload_dir_path) else [
+            file for file in os.listdir(session_upload_dir_path) if file.endswith('.pdf')]
         return render_template('index.html', form=form,
-                               uploaded_files=[
-                                   file
-                                   for file in os.listdir(session_upload_dir_path)
-                                   if file.endswith('.pdf')],
-                               test_form=test_form)
-
-    @main.route('/stupid')
-    def stupid():
-        stupid_background_task.delay()
-        return redirect(url_for('main.index'))
-
+                               uploaded_files=uploaded_files,
+                               compress_form=compress_form)
 
     @main.route('/about')
     def about():
